9e133f9a8489c6142095c5fc6fd27c74dda861a3
liren <327381633@qq.com>
2025-03-24 22:12:39 +0800

    feat: prisma installation and setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,6 @@ import ProductList from "@/components/shared/product/product-list";
 import sampleData from "@/db/sample-data";
 
 const HomePage = () => {
-  console.log(sampleData);
   return (
     <>
       <ProductList
